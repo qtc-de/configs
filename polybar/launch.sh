33d3d0d4e8dc35d648945cd204d7ce86aca21af7
Tobias Neitzel <secure@tneitzel.eu>
2022-12-09 06:27:39 +0100

    Update polybar launcher script
    Updated the polybar launcher script to make the tray always appearing on
    the primary monitor.

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,12 +6,23 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-if type "xrandr"; then
-  for m in $(polybar -m | cut -d":" -f1); do
-    MONITOR=$m polybar --reload mybar &
-  done
+if type 'xrandr'; then
+
+    # The first bar loaded gets the tray. Make sure it is on the
+    # primary monitor and load bars on other monitors with a delay
+    PRIMARY=$(polybar -m | grep 'primary' | cut -d':' -f1);
+
+    if [ -n "${PRIMARY}" ]; then
+        MONITOR=$m polybar --reload mybar &
+        sleep 1
+    fi
+
+    for m in $(polybar -m | grep -v 'primary' | cut -d':' -f1); do
+        MONITOR=$m polybar --reload mybar &
+    done
+
 else
-  polybar --reload mybar &
+    polybar --reload mybar &
 fi
 
-echo "Bars launched..."
+echo '[+] Bars launched...'
